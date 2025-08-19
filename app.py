9884560fc54a019c373c64f7e568a93541258c18
junhua <junhua.viva@gmail.com>
2025-08-19 17:06:00 +0800

    UI is ready

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.write("Hello world!")
+st.title("SIM STUDY PLANNING APP")
+
+option_school = st.selectbox(
+    "Your school",
+    ("UOL", "UOW", "RMIT"),
+    key="school"
+)
+
+
+option_degree = st.selectbox(
+    "Your degree",
+    ("Computer Science", "Business Management", "Data Science", "Finance"),
+    key="degree"
+)
+
+
+option_year = st.selectbox(
+    "Your year",
+    ("Year 1", "Year 2", "Year 3", "Pre-U"),
+    key="year"
+)
+
+if st.button("Generate Plan!"):
+    st.markdown("*Streamlit* is **really** ***cool***.")
